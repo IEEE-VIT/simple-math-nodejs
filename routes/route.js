ef4cf6c6c15994e892d78cc20664498596bf5342
Phernandoe <fbarreto@ithaca.edu>
2019-09-27 15:30:35 -0400

    /add route & tests

--- routes/route.js
+++ routes/route.js
@@ -6,8 +6,24 @@ router.get('/check', (req, res) => {
     res.send("Congratulations! Your app works! :)");
 })
 
-router.post('add', (req, res) => {
+router.post('/add', (req, res) => {
     // Add logic here
+
+    //Checks if arguments exist
+    if (req.body.x && req.body.y) {
+
+        //Checks if arguments are numbers
+        if ((!isNaN(req.body.x)) && (!isNaN(req.body.y)) ) {
+            const sum = parseInt(req.body.x) + parseInt(req.body.y);
+            res.status(200).send(`${req.body.x} + ${req.body.y} = ${sum}`);
+        } else {
+            res.status(400).send('Both arguments must be numbers');
+        }
+
+    } else {
+        res.status(400).send('Both arguments must be provided');
+    }
+    
 })
 
 module.exports = router;
--- test/tests.js
+++ test/tests.js
@@ -20,4 +20,61 @@ describe("----------START TEST FOR app.js----------", () => {
             }
         })
     })
+
+    it("Checks the /add route with both arguments as numbers", (done) => {
+        // *Sends post data as JSON, e.g. 
+        // {
+        //    "x": 5,
+        //    "y" : 5
+        // }
+        
+        chai.request(app)
+        .post("/math/add")
+        .send({'x': 5, 'y': 5})
+        .end((err, res) => {
+            if (err) {
+                done(err)
+                process.exit(1)
+            } else {
+                res.text.should.equal("5 + 5 = 10");
+                done();
+            }
+        })
+
+    })
+
+    it("Checks the /add route with one argument as NaN", (done) => {
+        
+        chai.request(app)
+        .post("/math/add")
+        .send({'x': 'a', 'y': 5})
+        .end((err, res) => {
+            if (err) {
+                done(err)
+                process.exit(1)
+            } else {
+                res.text.should.equal("Both arguments must be numbers");
+                done();
+            }
+        })
+        
+    })
+
+    it("Checks the /add route with only one argument", (done) => {
+        
+        chai.request(app)
+        .post("/math/add")
+        .send({'x': 1})
+        .end((err, res) => {
+            if (err) {
+                done(err)
+                process.exit(1)
+            } else {
+                res.text.should.equal("Both arguments must be provided");
+                done();
+            }
+        })
+        
+    })
+
 })
