6555b8cf9f40fa8c7beca12b2bc99255e678704e
pierre <pierre.git@posteo.de>
2019-09-29 14:09:22 +0200

    added /mul route

--- routes/route.js
+++ routes/route.js
@@ -10,4 +10,17 @@ router.post('add', (req, res) => {
     // Add logic here
 })
 
+router.post('/mul', (req, res) => {
+    if (req.body.x && req.body.y) {
+        if ((!isNaN(req.body.x)) && (!isNaN(req.body.y))) {
+            const multi = parseInt(req.body.x) * parseInt(req.body.y);
+            res.status(200).send(`${req.body.x} * ${req.body.y} = ${multi}`);
+        } else {
+            res.status(400).send('Both arguments (x, y) must be numbers.');
+        }
+    } else {
+        res.status(400).send('Both arguments (x, y) must be provided.');
+    }
+})
+
 module.exports = router;
--- test/tests.js
+++ test/tests.js
@@ -20,4 +20,139 @@ describe("----------START TEST FOR app.js----------", () => {
             }
         })
     })
+
+    it("Checks the /mul route with both arguments as numbers", (done) => {
+        /* Sends post data as JSON,
+         * {
+         *   "x": 2,
+         *   "y": 7
+         * }
+         */
+
+        chai.request(app)
+        .post("/math/mul")
+        .send({'x': 2, 'y':7})
+        .end((err, res) => {
+            if (err) {
+                done(err);
+                process.exit(1);
+            } else {
+                res.text.should.equal("2 * 7 = 14");
+                done();
+            }
+        })
+
+    })
+
+    it("Checks the /mul route with only x argument", (done) => {
+        /* Sends post data as JSON,
+         * {
+         *   "x": 2,
+         * }
+         */
+
+        chai.request(app)
+        .post("/math/mul")
+        .send({'x': 2})
+        .end((err, res) => {
+            if (err) {
+                done(err);
+                process.exit(1);
+            } else {
+                res.text.should.equal("Both arguments (x, y) must be provided.");
+                done();
+            }
+        })
+
+    })
+
+    it("Checks the /mul route with only y argument", (done) => {
+        /* Sends post data as JSON,
+         * {
+         *   "y": 2,
+         * }
+         */
+
+        chai.request(app)
+        .post("/math/mul")
+        .send({'y': 2})
+        .end((err, res) => {
+            if (err) {
+                done(err);
+                process.exit(1);
+            } else {
+                res.text.should.equal("Both arguments (x, y) must be provided.");
+                done();
+            }
+        })
+
+    })
+
+    it("Checks the /mul route without arguments", (done) => {
+        /* Sends post data as JSON,
+         * {
+         * }
+         */
+
+        chai.request(app)
+        .post("/math/mul")
+        .send({})
+        .end((err, res) => {
+            if (err) {
+                done(err);
+                process.exit(1);
+            } else {
+                res.text.should.equal("Both arguments (x, y) must be provided.");
+                done();
+            }
+        })
+
+    })
+
+    it("Checks the /mul route with x argument as NaN", (done) => {
+        /* Sends post data as JSON,
+         * {
+         *   "x": "p",
+         *   "y": 7
+         * }
+         */
+
+        chai.request(app)
+        .post("/math/mul")
+        .send({'x': 'p', 'y':7})
+        .end((err, res) => {
+            if (err) {
+                done(err);
+                process.exit(1);
+            } else {
+                res.text.should.equal("Both arguments (x, y) must be numbers.");
+                done();
+            }
+        })
+
+    })
+
+    it("Checks the /mul route with y argument as NaN", (done) => {
+        /* Sends post data as JSON,
+         * {
+         *   "x": 2,
+         *   "y": "p"
+         * }
+         */
+
+        chai.request(app)
+        .post("/math/mul")
+        .send({'x': 2, 'y':'p'})
+        .end((err, res) => {
+            if (err) {
+                done(err);
+                process.exit(1);
+            } else {
+                res.text.should.equal("Both arguments (x, y) must be numbers.");
+                done();
+            }
+        })
+
+    })
+
 })
