10de599bd71868f8f29e8681bc43fbc21655cf57
L04DB4L4NC3R <angadsharma1016@gmail.com>
2019-10-02 01:53:34 +0530

    added mul, test, and linting

--- test/tests.js
+++ test/tests.js
@@ -1,23 +1,43 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const app = require("../app");
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 const assert = require('assert');
 const should = require('should');
+const app = require('../app');
 
 chai.use(chaiHttp);
 
-describe("----------START TEST FOR app.js----------", () => {
-    it("Checks if the App is working", (done) => {
-        chai.request(app)
-        .get("/math/check")
-        .end((err, res) => {
-            if (err){
-                done(err)
-                process.exit(1)
-            } else {
-                res.text.should.equal("Congratulations! Your app works! :)");
-                done()
-            }
-        })
-    })
-})
+describe('----------START TEST FOR app.js----------', () => {
+  it('Checks if the App is working', (done) => {
+    chai.request(app)
+      .get('/math/check')
+      .end((err, res) => {
+        if (err) {
+          done(err);
+          process.exit(1);
+        } else {
+          res.text.should.equal('Congratulations! Your app works! :)');
+          done();
+        }
+      });
+  });
+
+
+  it('Checks if the multiplication route is working', (done) => {
+    chai.request(app)
+      .post('/math/mul')
+      .send({
+        param1: 2,
+        param2: 3,
+      })
+      .end((err, res) => {
+        if (err) {
+          done(err);
+          process.exit(1);
+        } else {
+          console.log(res);
+          assert(res.body.result === 6);
+          done();
+        }
+      });
+  });
+});
