e2f49be882a3e830c4f342013b894a1cf0cacc75
tjlerias <tjlerias4@gmail.com>
2019-10-02 09:13:58 +0800

    add subtract route

--- routes/route.js
+++ routes/route.js
@@ -10,4 +10,15 @@ router.post('add', (req, res) => {
     // Add logic here
 })
 
+router.post('subtract', (req, res) => {
+    try {
+        const {param1, param2} = req.body.data;
+        const difference = parseFloat(param1) - parseFloat(param2);
+        res.status(200).send({ result: difference });
+    } catch (error) {
+        res.status(400).send(error);
+    }
+    
+})
+
 module.exports = router;
