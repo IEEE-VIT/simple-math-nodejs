f19304ed03aa17b7f3cb4c8b4f584fa57e0bce9b
Rui Magalhães <rui@RuiMCPRO.home>
2019-10-06 17:44:49 +0100

    Add /math/root route for calculating roots; add unit tests for this route

--- routes/route.js
+++ routes/route.js
@@ -24,4 +24,36 @@ router.post("/power", (req, res) => {
     });
 });
 
+router.post("/root", (req, res) => {
+    try {
+        let param1 = req.body.data.param1;
+        let param2 = req.body.data.param2;
+
+        if (isNaN(param1) || isNaN(param2)) {
+            throw new Error;
+        }
+
+        let result = Math.pow(param1, 1/param2);
+
+        res.status(200).json({
+            result: result,
+            meta: {
+                success: true,
+                message: `Calculated root of ${param1} and degree ${param2}`,
+                code: 200
+            }
+        });
+
+    } catch (e) {
+        res.status(400).json({
+            result: null,
+            meta: {
+                success: false,
+                message: "Bad request, format should be: {data: {param1: number, param2: degree}}",
+                code: 400
+            }
+        });
+    }
+});
+
 module.exports = router;
