0f48f0981652ad79f2a97b52db77aac63f3e75d2
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2019-10-08 00:15:17 +0530

    Revert "Added route: POST /log"
    This reverts commit 2b95f973a778798e6428021d830c6d5786e5228e.

--- routes/route.js
+++ routes/route.js
@@ -42,22 +42,4 @@ router.post("/nthroot", (req, res) => {
     });
 });
 
-router.post("/log", (req, res) => {
-
-    // param1 - Number
-    // param2 - n in log to the base n. This is optional. If you don't put it, then, it's going to be natural log
-
-    let param1 = req.body.param1;
-    let param2 = req.body.param2;
-
-    res.json({
-        result: param2 ? Math.log(param1)/Math.log(param2) : Math.log(param1),
-        meta: {
-            success: true,
-            message: `Calculated log to the base ${param2} of ${param1}`,
-            code: 200
-        }
-    });
-});
-
 module.exports = router;
