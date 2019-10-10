5f73e19ce35f8fb8b278ecdcfa883296861af921
Arushit Mudgal <amudgal@akamai.com>
2019-10-10 16:27:52 +0530

    Fixing formatting. Adding floor functionality

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
-app.use('/math', require('./routes/route'));
+app.use("/math", require("./routes/route"));
 
 app.listen(PORT, () => {
-    console.log("App started successfully on PORT: ", PORT);
-})
+  console.log("App started successfully on PORT: ", PORT);
+});
 
 module.exports = app;
