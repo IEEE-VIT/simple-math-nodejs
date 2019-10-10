041ade77994c9e2d6790689a9f8570498ce71b08
Arushit Mudgal <amudgal@akamai.com>
2019-10-10 16:18:05 +0530

    Fixing formatting. Adding ceil function

--- routes/route.js
+++ routes/route.js
@@ -1,64 +1,88 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/check', (req, res) => {
-    res.send("Congratulations! Your app works! :)");
-})
+router.get("/check", (req, res) => {
+  res.send("Congratulations! Your app works! :)");
+});
 
-router.post('add', (req, res) => {
-    // Add logic here
-})
+router.post("add", (req, res) => {
+  // Add logic here
+});
 
 router.post("/power", (req, res) => {
-    let param1 = req.body.param1;
-    let param2 = req.body.param2;
+  let param1 = req.body.param1;
+  let param2 = req.body.param2;
 
-    res.json({
-        result: Math.pow(param1, param2),
-        meta: {
-            success: true,
-            message: `Calculated ${param1} raised to the power ${param2}`,
-            code: 200
-        }
-    });
+  res.json({
+    result: Math.pow(param1, param2),
+    meta: {
+      success: true,
+      message: `Calculated ${param1} raised to the power ${param2}`,
+      code: 200
+    }
+  });
 });
 
 router.post("/factorial", (req, res) => {
+  const { param1 } = req.body;
+
+  try {
+    let result = parseInt(param1, 10);
+
+    if (result < 0) {
+      return res.json({
+        meta: {
+          success: false,
+          message: `${param1} is a negative number`,
+          code: 400
+        }
+      });
+    }
+    for (let counter = result - 1; counter > 0; counter--) {
+      result *= counter;
+    }
+    res.json({
+      result,
+      meta: {
+        success: true,
+        message: `Calculated ${param1} factorial`,
+        code: 200
+      }
+    });
+  } catch (err) {
+    res.json({
+      meta: {
+        success: false,
+        message: err.message,
+        code: 400
+      }
+    });
+  }
+});
+
+router.post("/ceil", (req, res) => {
+  try {
     const { param1 } = req.body;
 
-    try {
-        let result = parseInt(param1, 10);
-
-        if (result < 0) {
-            return res.json({
-                meta: {
-                    success: false,
-                    message: `${param1} is a negative number`,
-                    code: 400
-                }
-            });
-        }
-        for (let counter = result - 1; counter > 0; counter--) {
-            result *= counter;
-        }
-        res.json({
-            result,
-            meta: {
-                success: true,
-                message: `Calculated ${param1} factorial`,
-                code: 200
-            }
-        });
-    } catch (err) {
-        res.json({
-            meta: {
-                success: false,
-                message: err.message,
-                code: 400
-            }
-        });
-    }
+    let result = Math.ceil(parseFloat(param1, 10));
+    res.json({
+      result,
+      meta: {
+        success: true,
+        message: `Calculated ${param1} ceil`,
+        code: 200
+      }
+    });
+  } catch (err) {
+    res.json({
+      meta: {
+        success: false,
+        message: err.message,
+        code: 400
+      }
+    });
+  }
 });
 
 module.exports = router;
