ca5df1da5d50ef4012850c915cc3ab51e3b1ee35
LordOfDexterity <54269414+LordOfDexterity@users.noreply.github.com>
2019-10-16 18:17:39 +0530

    PULL_REQUEST_TEMPLATE.md
    changed name of param1 to darthvader ;)

--- routes/route.js
+++ routes/route.js
@@ -11,30 +11,30 @@ router.post("add", (req, res) => {
 });
 
 router.post("/power", (req, res) => {
-  let param1 = req.body.param1;
+  let darthvader = req.body.darthvader;
   let param2 = req.body.param2;
-
+  
   res.json({
-    result: Math.pow(param1, param2),
+    result: Math.pow(darthvader, param2),
     meta: {
       success: true,
-      message: `Calculated ${param1} raised to the power ${param2}`,
+      message: `Calculated ${} raised to the power ${param2}`,
       code: 200
     }
   });
 });
 
 router.post("/factorial", (req, res) => {
-  const { param1 } = req.body;
+  const { darthvader } = req.body;
 
   try {
-    let result = parseInt(param1, 10);
+    let result = parseInt(darthvader, 10);
 
     if (result < 0) {
       return res.json({
         meta: {
           success: false,
-          message: `${param1} is a negative number`,
+          message: `${darthvader} is a negative number`,
           code: 400
         }
       });
@@ -46,7 +46,7 @@ router.post("/factorial", (req, res) => {
       result,
       meta: {
         success: true,
-        message: `Calculated ${param1} factorial`,
+        message: `Calculated ${darthvader} factorial`,
         code: 200
       }
     });
@@ -63,14 +63,14 @@ router.post("/factorial", (req, res) => {
 
 router.post("/ceil", (req, res) => {
   try {
-    const { param1 } = req.body;
+    const { darthvader } = req.body;
 
-    let result = Math.ceil(parseFloat(param1, 10));
+    let result = Math.ceil(parseFloat(darthvader, 10));
     res.json({
       result,
       meta: {
         success: true,
-        message: `Calculated ${param1} ceil`,
+        message: `Calculated ${darthvader} ceil`,
         code: 200
       }
     });
